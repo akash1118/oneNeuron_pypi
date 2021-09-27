3ead04a617bbd7cc4fbf0a4d29c250ec6a5bf757
Akash Verma <akashlucky18@gmail.com>
2021-09-27 23:49:41 +0530

    fixed syntax errors

--- setup.py
+++ setup.py
@@ -22,12 +22,13 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        install_requires=[
-            "numpy",
-            "tqdm"
-        ]
     ],
-    package_dir={"": "src"},
+    package_dir =  {"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
+     install_requires=[
+        "numpy",
+        "tqdm"
+    ]
 )
+       
